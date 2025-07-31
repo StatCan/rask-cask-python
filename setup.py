304683160dc0e05fae73294040c31c21b7b9bdd1
Bryan Paget <8212170+bryanpaget@users.noreply.github.com>
2025-07-31 16:25:44 -0400

    initial commit
    migrated from https://gccode.ssc-spc.gc.ca/stc-srgd-rlpc/rask-cask-python

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(name='rask_cask',
+      author='Diego Ripley',
+      author_email='diego.ripley@canada.ca',
+      version='0.1',
+      packages=find_packages(),
+      include_package_data=True
+      )
